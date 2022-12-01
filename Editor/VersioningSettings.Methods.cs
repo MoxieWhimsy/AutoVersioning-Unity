c716c7547d1994bc4ff9ef023954d198f3335b86
Mox <977898+MoxieWhimsy@users.noreply.github.com>
2022-11-30 19:41:41 -0500

    refactor: extract methods from VersioningSettings

---
+++ Editor/VersioningSettings.Methods.cs
@@ -0,0 +1,63 @@
+#if UNITY_EDITOR
+using System.Collections.Generic;
+using System.Linq;
+using RedBlueGames;
+
+namespace Build.Editor
+{
+    public partial class VersioningSettings
+    {
+        private int CountBothMinorAndPatch(IEnumerable<string> lines)
+            => lines.Count(line => UnionRegex.IsMatch(line));
+
+        /// <summary>
+        /// Returns the number of commits on main up to the commit where this branch connects to main.
+        /// </summary>
+        private int CountCommitsOnMainToBranch()
+        {
+            var commitId = GetWhichCommitBranchesFromMain();
+            return int.TryParse(Git.Run($@"rev-list --count {commitId}"), out var number) ? number : 0;
+        }
+
+        /// <summary>
+        /// Return the number of commits since the current branch forks from the main branch. 
+        /// </summary>
+        private int CountCommitsSinceMain()
+            => int.Parse(Git.Run($@"rev-list --count {MainBranchName}..HEAD"));
+
+        private int CountMinorThenPatch(string[] lines)
+        {
+            var patch = GetMinorAndThenPatch(lines, out var minor);
+            return MaxPatchesPerMinor * minor + patch;
+        }
+
+        private int CountMainAndBranch()
+        {
+            var mainToBranchPt = CountCommitsOnMainToBranch();
+            var headFromBranchPt = CountCommitsSinceMain();
+            return BranchCommitLimit * mainToBranchPt + headFromBranchPt;
+        }
+
+        internal int GetBuildNumber(string[] lines)
+        {
+            return commitCountingStyle switch
+            {
+                Versioning.NumberType.BothMinorAndPatch => CountBothMinorAndPatch(lines),
+                Versioning.NumberType.MinorThenPatch => CountMinorThenPatch(lines),
+                Versioning.NumberType.MainAndBranch => CountMainAndBranch(),
+                _ => 0
+            } + numberOffset;
+        }
+
+        internal int GetMinorAndThenPatch(string[] lines, out int minor)
+        {
+            minor = lines.Count(line => MinorRegex.IsMatch(line));
+            lines = lines.TakeWhile(line => !MinorRegex.IsMatch(line)).ToArray();
+            return lines.Count(line => PatchRegex.IsMatch(line));
+        }
+
+        private string GetWhichCommitBranchesFromMain()
+            => Git.Run($@"merge-base {MainBranchName} HEAD");
+    }
+}
+#endif
--- Editor/VersioningSettings.cs
+++ Editor/VersioningSettings.cs
@@ -1,14 +1,12 @@
 #if UNITY_EDITOR
-using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
-using RedBlueGames;
 using UnityEditor;
 using UnityEngine;
 
 namespace Build.Editor
 {
-    public class VersioningSettings : ScriptableObject
+    public partial class VersioningSettings : ScriptableObject
     {
         [SerializeField] private Versioning.VersionControl _versionControlSystem = Versioning.VersionControl.Git;
         public Versioning.VersionControl VersionControlSystem => _versionControlSystem;
@@ -51,58 +49,7 @@ namespace Build.Editor
         private const string DefaultSettingsFolder = "Assets/Editor/Settings";
         private static readonly string DefaultSettingsPath = $"{DefaultSettingsFolder}/{nameof(VersioningSettings)}.asset";
 
-        
-        private int CountBothMinorAndPatch(IEnumerable<string> lines) 
-            => lines.Count(line => UnionRegex.IsMatch(line));
-        
-        /// <summary>
-        /// Returns the number of commits on main up to the commit where this branch connects to main.
-        /// </summary>
-        public int CountCommitsOnMainToBranch()
-        {
-            var commitId = GetWhichCommitBranchesFromMain();
-            return int.TryParse(Git.Run($@"rev-list --count {commitId}"), out var number) ? number : 0;
-        }
 
-        /// <summary>
-        /// Return the number of commits since the current branch forks from the main branch. 
-        /// </summary>
-        private int CountCommitsSinceMain()
-            => int.Parse(Git.Run($@"rev-list --count {MainBranchName}..HEAD"));
-        
-        private int CountMinorThenPatch(string[] lines)
-        {
-            var patch = GetMinorAndThenPatch(lines, out var minor);
-            return MaxPatchesPerMinor * minor + patch;
-        }
-        
-        private int CountMainAndBranch()
-        {
-            var mainToBranchPt = CountCommitsOnMainToBranch();
-            var headFromBranchPt = CountCommitsSinceMain();
-            return BranchCommitLimit * mainToBranchPt + headFromBranchPt;
-        }
-        
-        internal int GetBuildNumber(string[] lines)
-        {
-            return commitCountingStyle switch
-            {
-                Versioning.NumberType.BothMinorAndPatch => CountBothMinorAndPatch(lines),
-                Versioning.NumberType.MinorThenPatch => CountMinorThenPatch(lines),
-                Versioning.NumberType.MainAndBranch => CountMainAndBranch(),
-                _ => 0
-            } + numberOffset;
-        }
-        public int GetMinorAndThenPatch(string[] lines, out int minor)
-        {
-            minor = lines.Count(line => MinorRegex.IsMatch(line));
-            lines = lines.TakeWhile(line => !MinorRegex.IsMatch(line)).ToArray();
-            return lines.Count(line => PatchRegex.IsMatch(line));
-        }
-        
-        public string GetWhichCommitBranchesFromMain()
-            => Git.Run($@"merge-base {MainBranchName} HEAD");
-        
         internal static VersioningSettings GetOrCreate()
         {
             var foundPaths = GetSettingsPaths();
